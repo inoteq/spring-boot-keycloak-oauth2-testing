891f6d8447bc17b032b63beeb84c3cfed5700c96
Alexander Kusmin <alexander.kusmin@inoteq.com>
2024-01-30 08:37:40 +0100

    Updates tests

--- src/test/kotlin/DemoTest.kt
+++ src/test/kotlin/DemoTest.kt
@@ -1,5 +1,14 @@
 import com.example.demo.DemoApplication
+import com.microsoft.playwright.Browser
+import com.microsoft.playwright.BrowserContext
+import com.microsoft.playwright.Page
+import com.microsoft.playwright.Playwright
+import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
 import dasniko.testcontainers.keycloak.KeycloakContainer
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
@@ -16,14 +25,16 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @Testcontainers
 class DemoTest {
     companion object {
+        // Start a Keycloak instance with an import file for the demo realm
         @JvmStatic
         @Container
         private val keycloakContainer = KeycloakContainer().apply {
-            withRealmImportFile("demo-realm-realm.json")
+            withRealmImportFile("realm.json")
             portBindings = listOf("8081:8080")
             start()
         }
 
+        // Register the Keycloak issuer URL as a dynamic property for the Spring Boot application
         @JvmStatic
         @DynamicPropertySource
         private fun registerResourceServerIssuerProperty(registry: DynamicPropertyRegistry) {
@@ -32,63 +43,56 @@ class DemoTest {
             }
         }
 
-//        var playwright: Playwright? = null
-//        var browser: Browser? = null
-//        var context: BrowserContext? = null
-//        var page: Page? = null
+        // Playwright setup
+        private var playwright: Playwright? = null
+        private var browser: Browser? = null
+        private var context: BrowserContext? = null
+        private var page: Page? = null
 
-//        @JvmStatic
-//        @BeforeAll
-//        fun launchBrowser() {
-//            playwright = Playwright.create()
-//            browser = playwright?.chromium()?.launch()
-//        }
+        @JvmStatic
+        @BeforeAll
+        fun launchBrowser() {
+            playwright = Playwright.create()
+            browser = playwright?.chromium()?.launch()
+        }
 
-//        @JvmStatic
-//        @AfterAll
-//        fun closeBrowser() {
-//            playwright?.close()
-//        }
+        @JvmStatic
+        @AfterAll
+        fun closeBrowser() {
+            playwright?.close()
+        }
+    }
+
+    @BeforeEach
+    fun createContextAndPage() {
+        context = browser?.newContext()
+        page = context?.newPage()
+    }
+
+    @AfterEach
+    fun closeContext() {
+        context?.close()
     }
 
     @Test
-    fun contextLoads() {
-        println("Hello World!")
+    fun publicEndpointTest() {
+        page?.navigate("http://localhost:8080/public")
+        assertThat(page?.locator("body")).hasText("Hello from a public endpoint!")
     }
 
-//    @BeforeEach
-//    fun createContextAndPage() {
-//        context = browser?.newContext()
-//        page = context?.newPage()
-//    }
-//
-//    @AfterEach
-//    fun closeContext() {
-//        context?.close()
-//    }
+    @Test
+    fun privateEndpointTest() {
+        // Unable to access private endpoint without logging in and redirects to login page
+        page?.navigate("http://localhost:8080/private")
 
-//    @Test
-//    fun publicEndpointTest() {
-//        println("Test 1")
-//        page?.navigate("http://localhost:8080/public")
-//        println("Test 2")
-//        assertThat(page?.textContent("body")).isEqualTo("Hello from a public endpoint!")
-//        println("Test 3")
-//    }
-//
-//    @Test
-//    fun privateEndpointTest() {
-//        // Unable to access private endpoint without logging in and redirects to login page
-//        page?.navigate("http://localhost:8080/private")
-//
-//        // Login with example user 'John Doe' by filling the login form and submitting it
-//        page?.fill("#username", "john.doe")
-//        page?.fill("#password", "password")
-//        page?.locator("input[type=submit]")?.click()
-//        // Wait for redirect to previously requested private endpoint
-//        page?.waitForURL("http://localhost:8080/private?continue")
-//
-//        // Check for expected content on private endpoint
-//        assertThat(page?.textContent("body")).isEqualTo("Hello from a private endpoint!")
-//    }
+        // Login with example user 'John Doe' by filling the login form and submitting it
+        page?.fill("#username", "john.doe")
+        page?.fill("#password", "password")
+        page?.locator("input[type=submit]")?.click()
+        // Wait for redirect to previously requested private endpoint
+        page?.waitForURL("http://localhost:8080/private?continue")
+
+        // Check for expected content on private endpoint
+        assertThat(page?.locator("body")).hasText("Hello from a private endpoint!")
+    }
 }
